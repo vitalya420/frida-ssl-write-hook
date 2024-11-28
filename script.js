95b10f661b7c7f7f34a040355c267a82dba58d7c
Vitaliy <67874485+vitalya420@users.noreply.github.com>
2024-11-28 22:39:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const libssl = Process.getModuleByName('libssl.so');
 
-var SSL_write = libssl.getExportByName('SSL_write');
+
+const SSL_write = libssl.getExportByName('SSL_write');
+const SSL_read = libssl.getExportByName('SSL_read');
+const SSL_set_fd = libssl.getExportByName('SSL_set_fd');
+
+
+
+Interceptor.attach(SSL_read, {
+    onEnter(args){
+        this.sslCtx = args[0];
+        this.buf = args[1];
+        this.num = args[2].toInt32();
+    },
+    onLeave(byte_read){
+        byte_read = byte_read.toInt32();
+        if (byte_read > 0){            
+            const bytes = this.buf.readByteArray(byte_read);
+            send({func: 'SSL_read', ssl: this.sslCtx}, bytes);
+        }
+    }
+});
 
 Interceptor.attach(SSL_write, {
     onEnter: function (args) {
@@ -11,9 +31,9 @@ Interceptor.attach(SSL_write, {
         for (var i = 0; i < num; i++) {
             byteArray.push(buf.add(i).readU8());
         }
-        Thread.backtrace(this.context, Backtracer.FUZZY).map(addr => {
-            console.log('[!]', DebugSymbol.fromAddress(addr));
-        });
-        // send({ buf: byteArray });
+        // Thread.backtrace(this.context, Backtracer.FUZZY).map(addr => {
+        //     console.log('[!]', DebugSymbol.fromAddress(addr));
+        // });
+        // send({ SSL_write: byteArray });
     }
 });
